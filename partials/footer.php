06972cb46b425f4db81ddab4652de56df59906c7
Jéremie <jeremie.seynhaeve@gmail.com>
2023-11-20 13:11:24 +0100

    Update footer.php
    Mise à jour url

--- partials/footer.php
+++ partials/footer.php
@@ -3,13 +3,13 @@
     <div class="w-full mx-auto">
         <ul class="flex flex-wrap justify-between items-center text-sm font-medium text-gray-500">
             <li>
-                <a href="#">Linkedin</a>
+                <a href="https://github.com/angerfistos/annuaire.git">GitHub</a>
             </li>
             <li>
-                <a href="#">GitHub</a>
+                <a href="https://trello.com/invite/b/zxr6Ejzh/ATTI729ffe53007a1f4347618500769f803aE96FF0C3/web-design-development">Trello</a>
             </li>
             <li>
-                <a href="#">Trello</a>
+                <a href="https://www.figma.com/file/HNmiwmr1isgpF7pQM2GlFn/Untitled?type=design&node-id=0%3A1&mode=design&t=hqPS7r7wiYD70gDi-1">Figma</a>
             </li>
         </ul>
     </div>
